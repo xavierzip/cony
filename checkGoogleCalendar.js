bd718a29717da32ffceaf38270178c2a53a93888
Xavier Mao <xavier.m.zip@gmail.com>
2015-09-08 11:45:40 +0800

    updated google canlendar search to work with more calendars

--- checkGoogleCalendar.js
+++ checkGoogleCalendar.js
@@ -118,7 +118,32 @@ function listEvents(auth) {
     if (events.length == 0) {
       console.log('No upcoming events found.');
     } else {
-      console.log('Upcoming 10 events:');
+      console.log('Upcoming china holidays in next 10 days:');
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+        console.log('%s - %s', start, event.summary);
+      }
+    }
+  });
+  calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: moment().format(),
+    timeMax: moment().add(10, 'days').format(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var events = response.items;
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+    } else {
+      console.log('Upcoming personal events in next 10 days:');
       for (var i = 0; i < events.length; i++) {
         var event = events[i];
         var start = event.start.dateTime || event.start.date;
